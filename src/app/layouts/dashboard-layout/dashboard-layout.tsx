ce4715f316bae5ef88848a20c5fa32f32f21c366
Navatusein <boghdan.kutsulima@gmail.com>
2024-09-30 18:21:39 +0300

    Add dashboard layout context

--- src/app/layouts/dashboard-layout/dashboard-layout.tsx
+++ src/app/layouts/dashboard-layout/dashboard-layout.tsx
@@ -1,24 +1,27 @@
-import {FC} from "react";
+import {FC, useState} from "react";
 import {Layout, theme} from "antd";
 import {Outlet} from "react-router-dom";
 import {SideMenu} from "@/widgets/side-menu";
 import {Header} from "@/widgets/header";
+import {Footer} from "@/widgets/footer";
 
 const {Content} = Layout;
 
 const DashboardLayout: FC = () => {
   const {token: {paddingLG}} = theme.useToken();
 
+  const [pageTitle, setPageTitle] = useState("Home")
 
   return (
     <Layout>
       <SideMenu/>
       <Layout>
-        <Header pageTitle={"Header"}/>
+        <Header pageTitle={pageTitle}/>
         <Layout style={{overflowY: "auto"}}>
           <Content style={{padding: `${paddingLG}px`, minHeight: "fit-content"}}>
-            <Outlet/>
+            <Outlet context={{setPageTitle}}/>
           </Content>
+          <Footer/>
         </Layout>
       </Layout>
     </Layout>
