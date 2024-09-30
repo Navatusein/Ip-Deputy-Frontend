2aa32577eefa77dab697823ddd6db29dfba1142a
Navatusein <boghdan.kutsulima@gmail.com>
2024-09-30 20:14:23 +0300

    Fix schedule page for mobile resolution

--- src/pages/schedule/ui/schedule-page/schedule-page.tsx
+++ src/pages/schedule/ui/schedule-page/schedule-page.tsx
@@ -197,19 +197,21 @@ const SchedulePage: FC = () => {
   return (
     <Card bordered={false}>
       <Space direction="vertical" style={{width: "100%"}}>
-        <Segmented
-          options={[
-            {label: "Monday", value: 1},
-            {label: "Tuesday", value: 2},
-            {label: "Wednesday", value: 3},
-            {label: "Thursday", value: 4},
-            {label: "Friday", value: 5},
-            {label: "Saturday", value: 6},
-            {label: "Sunday", value: 7}
-          ]}
-          value={dayOfWeekId}
-          onChange={(value) => {setDayOfWeekId(value)}}
-        />
+        <div style={{overflowX: "auto"}}>
+          <Segmented
+            options={[
+              {label: "Monday", value: 1},
+              {label: "Tuesday", value: 2},
+              {label: "Wednesday", value: 3},
+              {label: "Thursday", value: 4},
+              {label: "Friday", value: 5},
+              {label: "Saturday", value: 6},
+              {label: "Sunday", value: 7}
+            ]}
+            value={dayOfWeekId}
+            onChange={(value) => {setDayOfWeekId(value)}}
+          />
+        </div>
         <DataTable
           columns={columns}
           data={schedule}
